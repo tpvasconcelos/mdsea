188cce29bd2c0402da6ea2289515007e7d8ce87f
tpvasconcelos <tomasvasconcelos1@gmail.com>
2018-12-19 21:59:55 +0100

    improve documentation

--- tests/cleanup.py
+++ tests/cleanup.py
@@ -1,8 +1,10 @@
 #!/usr/local/bin/python
 # coding: utf-8
+"""
+Remove the entire simulation directory.
+
+"""
 from tests import mdsea as md
 
 sm = md.SysManager.load(simid="_mdsea_testsimulation")
-
-# Remove the entire simulation directory.
 sm.delete()
