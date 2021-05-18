b80eafc73f873001df326f0e777b968c3a258a2f
Tomas Pereira de Vasconcelos <tomasvasconcelos1@gmail.com>
2021-05-18 23:42:29 +0100

    fix typo in mayavi code (z coord should be 3rd index)

--- mdsea/vis/mayavi.py
+++ mdsea/vis/mayavi.py
@@ -11,21 +11,22 @@ class MayaviAnimation(Vis):
     def __init__(self, sm: SysManager, frame_step: int = 1) -> None:
         super().__init__(sm, frame_step)
 
-        # Disable the rendering, to get bring up the figure quicker:
         figure = mlab.gcf()
         mlab.clf()
+
+        # Disable the rendering, to get bring up the figure quicker
         figure.scene.disable_render = True
 
         for i in range(self.sm.NUM_PARTICLES):
             mlab.points3d(
                 self.r_vecs[0][i][0],
                 self.r_vecs[0][i][1],
-                self.r_vecs[0][i][1],
+                self.r_vecs[0][i][2],
                 color=speed2color(speed=self.speeds[0][i], speed_limit=self.maxspeed, alpha=False),
                 resolution=8 * 3,
             )
 
-        # Every object has been created, we can reenable the rendering.
+        # Every object has been created, we can re-enable the rendering.
         figure.scene.disable_render = False
 
         mlab.show()
